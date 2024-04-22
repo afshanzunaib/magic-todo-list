fc54f9011511113e32e31f645d91ab6013e62554
afshanzunaib <163379124+afshanzunaib@users.noreply.github.com>
2024-04-21 23:59:06 -0700

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todolist = [];
+let condition = true;
+while (condition) {
+    const Add = await inquirer.prompt([{
+            message: "what as you want add in your todolist", type: "input", name: "todolist"
+        },
+        { message: "Do you  want to add more items", type: "confirm", name: "confirm", default: "false" },
+    ]);
+    todolist.push(Add.todolist);
+    condition = Add.confirm;
+    console.log(todolist);
+}
